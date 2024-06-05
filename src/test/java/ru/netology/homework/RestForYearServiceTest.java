931fbeca464df366da6206a083d6c2e40ba06852
Nikolay Martoplyas <kolyan94martoplyas@gmail.com>
2024-06-05 22:12:10 +0300

    ParameterizedTest

--- src/test/java/ru/netology/homework/RestForYearServiceTest.java
+++ src/test/java/ru/netology/homework/RestForYearServiceTest.java
@@ -2,17 +2,20 @@ package ru.netology.homework;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 
 public class RestForYearServiceTest {
-    @Test
-    public void numberOfHolidaysPerYear () {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/rest.csv")
+    public void numberOfHolidaysPerYear (int income, int expenses, int threshold, int expected ) {
         RestForYearService service = new RestForYearService();
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-
-        int expected = 3;
+//        int income = 10_000;
+//        int expenses = 3_000;
+//        int threshold = 20_000;
+//
+//        int expected = 3;
         int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
